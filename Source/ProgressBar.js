99385882c5a0b433a46e8f35d355ab666343ecec
David Walsh <davidwalsh83@gmail.com>
2009-11-22 14:30:05 -0600

    Adding author info

--- Source/ProgressBar.js
+++ Source/ProgressBar.js
@@ -1,3 +1,12 @@
+/*
+Script: ProgressBar
+
+License: MIT-style license.
+
+Copyright: Copyright (c) 2007-2009 [David Walsh](http://davidwalsh.name/).
+
+Author: David Walsh (http://davidwalsh.name)
+*/
 var ProgressBar = new Class({
 
 	//implements
