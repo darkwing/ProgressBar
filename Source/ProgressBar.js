8c5beb0b4f564c565ac68bb46147c81061be38a5
David Walsh <davidwalsh83@gmail.com>
2009-11-29 14:17:20 -0600

    Updating to 1.1

--- Source/ProgressBar.js
+++ Source/ProgressBar.js
@@ -1,11 +1,20 @@
 /*
-Script: ProgressBar
+---
+description:     ProgressBar
 
-License: MIT-style license.
+authors:
+  - David Walsh (http://davidwalsh.name)
 
-Copyright: Copyright (c) 2007-2009 [David Walsh](http://davidwalsh.name/).
+license:
+  - MIT-style license
 
-Author: David Walsh (http://davidwalsh.name)
+requires:
+  core/1.2.1:   '*'
+  more/1.2.1:   'Fx.*'
+
+provides:
+  - ProgressBar
+...
 */
 var ProgressBar = new Class({
 
